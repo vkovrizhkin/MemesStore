8d961399fe5ccc9a81c54adee57132c6057700bd
vladimir <vovanychkov95@gmail.com>
2019-08-21 20:42:49 +0300

    button

--- app/src/main/java/com/kovrizhkin/memesstore/LoginActivity.kt
+++ app/src/main/java/com/kovrizhkin/memesstore/LoginActivity.kt
@@ -2,11 +2,13 @@ package com.kovrizhkin.memesstore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        loginButton.startAnimation()
     }
 }
