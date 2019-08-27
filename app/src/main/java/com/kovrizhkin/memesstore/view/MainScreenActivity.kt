5df96e6a3c6f4635cf80911684551efc90cefc19
vladimir <vovanychkov95@gmail.com>
2019-08-27 20:18:17 +0300

    view pager with bottom bar

--- app/src/main/java/com/kovrizhkin/memesstore/view/MainScreenActivity.kt
+++ app/src/main/java/com/kovrizhkin/memesstore/view/MainScreenActivity.kt
@@ -2,6 +2,8 @@ package com.kovrizhkin.memesstore.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.kovrizhkin.memesstore.R
 import com.kovrizhkin.memesstore.view.adapters.MainViewPagerFragmentAdapter
 import kotlinx.android.synthetic.main.activity_main_screen.*
@@ -14,5 +16,53 @@ class MainScreenActivity : AppCompatActivity() {
 
         viewPager.adapter = MainViewPagerFragmentAdapter(supportFragmentManager)
 
+        syncViewPagerWithBottomBar(viewPager, bottomNavigationView)
+    }
+
+    private fun syncViewPagerWithBottomBar(viewPager: ViewPager, bottomBar: BottomNavigationView) {
+
+        bottomBar.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.mem_list_tab -> viewPager.currentItem = MEMES_LIST_FRAGMENT_INDEX
+                R.id.add_mem_tab -> viewPager.currentItem = ADD_MEM_FRAGMENT_INDEX
+                R.id.profile_tab -> viewPager.currentItem = PROFILE_FRAGMENT_INDEX
+            }
+            return@setOnNavigationItemSelectedListener true
+        }
+
+        viewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrollStateChanged(state: Int) {
+                // TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+                // TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onPageSelected(position: Int) {
+                when (position) {
+                    MEMES_LIST_FRAGMENT_INDEX -> bottomBar.menu.getItem(MEMES_LIST_FRAGMENT_INDEX)
+                        .isChecked = true
+                    ADD_MEM_FRAGMENT_INDEX -> bottomBar.menu.getItem(ADD_MEM_FRAGMENT_INDEX)
+                        .isChecked = true
+                    PROFILE_FRAGMENT_INDEX -> bottomBar.menu.getItem(PROFILE_FRAGMENT_INDEX)
+                        .isChecked = true
+                }
+            }
+
+        })
+
+    }
+
+    companion object {
+        const val MEMES_LIST_FRAGMENT_INDEX = 0
+
+        const val ADD_MEM_FRAGMENT_INDEX = 1
+
+        const val PROFILE_FRAGMENT_INDEX = 2
     }
 }
