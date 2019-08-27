b4d940b251489015fc6475a9b1c25ce1f97926fc
vladimir <vovanychkov95@gmail.com>
2019-08-27 15:31:40 +0300

    presenters interface

--- app/src/main/java/com/kovrizhkin/memesstore/presenters/PresenterContract.kt
+++ app/src/main/java/com/kovrizhkin/memesstore/presenters/PresenterContract.kt
@@ -1,9 +1,25 @@
 package com.kovrizhkin.memesstore.presenters
 
+import com.kovrizhkin.memesstore.model.memes.MemInfo
+
 interface PresenterContract {
 
     interface ILoginPresenter {
         fun onLogin(login: String, password: String)
+    }
+
+    interface IMemeListPresenter {
+        fun updateMemesList()
+        fun addToFavorites(mem: MemInfo)
+        fun getFavoriteIds(): List<Int>
+    }
+
+    interface IProfilePresenter {
+        fun logOut()
+    }
+
+    interface IAddMemPresenter {
+        fun saveMem(mem: MemInfo)
     }
 
 }
