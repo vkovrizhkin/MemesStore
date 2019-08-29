64d84b6c75c94d297655ae393cc92cf47066b025
vladimir <vovanychkov95@gmail.com>
2019-08-29 09:23:01 +0300

    fill view in

--- app/src/main/java/com/kovrizhkin/memesstore/utils/DateHelperUtil.kt
+++ app/src/main/java/com/kovrizhkin/memesstore/utils/DateHelperUtil.kt
@@ -1,12 +1,43 @@
 package com.kovrizhkin.memesstore.utils
 
+import com.kovrizhkin.memesstore.App
+import com.kovrizhkin.memesstore.R
+
 object DateHelperUtil {
 
-    fun getDaysAgo(milliSeconds: Long): Int {
+    private fun getDaysAgo(milliSeconds: Long): Int {
         // Create a calendar object that will convert the date and time value in milliseconds to date.
 
 
-        return ((System.currentTimeMillis() - milliSeconds*1000) / 1000 / 60 / 60 / 24).toInt()
+        return ((System.currentTimeMillis() - milliSeconds * 1000) / 1000 / 60 / 60 / 24).toInt()
+    }
+
+    fun getFormattedDaysAgo(milliSeconds: Long): String {
+
+        val days = getDaysAgo(milliSeconds)
+
+        return when (days) {
+            0 -> App.getContext()!!.getString(R.string.today)
+            1 -> App.getContext()!!.getString(R.string.yesterday)
+            else -> {
+                when (days / 10) {
+                    1 -> App.getContext()!!.getString(R.string.days_ago3, days)
+                    else -> {
+                        when (days % 10) {
+                            1 -> App.getContext()!!.getString(R.string.days_ago1, days)
+                            2, 3, 4 -> App.getContext()!!.getString(R.string.days_ago2, days)
+                            0, 5, 6, 7, 8, 9 -> App.getContext()!!.getString(
+                                R.string.days_ago3,
+                                days
+                            )
+                            else -> App.getContext()!!.getString(R.string.days_ago3, days)
+                        }
+                    }
+                }
+            }
+        }
+
+
     }
 
 }
--- app/src/main/java/com/kovrizhkin/memesstore/view/screens/DetailMemActivity.kt
+++ app/src/main/java/com/kovrizhkin/memesstore/view/screens/DetailMemActivity.kt
@@ -2,9 +2,11 @@ package com.kovrizhkin.memesstore.view.screens
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import com.kovrizhkin.memesstore.R
 import com.kovrizhkin.memesstore.model.memes.MemInfo
 import com.kovrizhkin.memesstore.utils.DateHelperUtil
+import kotlinx.android.synthetic.main.activity_detail_mem.*
 import java.time.Duration
 
 class DetailMemActivity : AppCompatActivity() {
@@ -15,10 +17,6 @@ class DetailMemActivity : AppCompatActivity() {
 
         val memInfo = intent.getSerializableExtra(MEM_EXTRA_KEY) as MemInfo
 
-
-
-        val date = DateHelperUtil.getDaysAgo(memInfo.createdDate)
-
         showMemDetails(memInfo)
 
 
@@ -26,9 +24,16 @@ class DetailMemActivity : AppCompatActivity() {
 
     private fun showMemDetails(memInfo: MemInfo) {
 
-        val date = DateHelperUtil.getDaysAgo(memInfo.createdDate)
+        val date = DateHelperUtil.getFormattedDaysAgo(memInfo.createdDate)
 
+        dateAgoText.text = date
+        descriptionText.text = memInfo.description
+        titleText.text = memInfo.title
 
+        Glide.with(this)
+            .load(memInfo.photoUtl)
+            .placeholder(R.drawable.ic_crop_original_white_24dp)
+            .into(image)
     }
 
     companion object {
